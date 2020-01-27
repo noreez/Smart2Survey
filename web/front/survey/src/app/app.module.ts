2c125b52f9478f858dee53a9b55bbbc97a1603f5
Demba <m.demba.dia@gmail.com>
2020-01-27 18:18:21 +0100

    init project angular with materialize-css

---
+++ web/front/survey/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { SubscribeComponent } from './pages/subscribe/subscribe.component';
+import { ConnectionComponent } from './pages/connection/connection.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomepageComponent,
+    SubscribeComponent,
+    ConnectionComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomepageComponent },
+      { path: 'subscribe', component: SubscribeComponent },
+      { path: 'connection', component: SubscribeComponent },
+
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
