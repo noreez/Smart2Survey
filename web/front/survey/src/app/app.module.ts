7ddc7e67a72dac00540ad185e622bde9fa2fd2bd
Demba <m.demba.dia@gmail.com>
2020-01-29 17:52:21 +0100

    update all design

--- web/front/survey/src/app/app.module.ts
+++ web/front/survey/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { ProfileComponent } from './pages/profile/profile.component';
       { path: '', component: HomepageComponent },
       { path: 'subscribe', component: SubscribeComponent },
       { path: 'connection', component: ConnectionComponent },
+      { path: 'profile', component: ProfileComponent },
 
     ])
   ],
